c821d529b1022efc793325a74e465e1766d75922
SashaBondar <albon10101983@gmail.com>
2017-10-22 17:50:32 +0300

    add coment to main method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld{
-	public String str = "Hello world!";
+	public static String str;
+	static {
+		str = "Hello world!";
+	}
 	public static void main(String[] args){
+		// Alpfa coment
 		System.out.println(str);
 	}
 }
